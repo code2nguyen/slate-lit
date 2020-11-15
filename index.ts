7ab010e0f79e4ba7ff1bb6ad8112478517df1a40
Nguyen Thai Vinh <code2nguyen@gmail.com>
2020-11-15 13:23:43 +0100

    prepare publish

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { SlateLit } from './src/slate-lit.component';
-export { RichTextEditor } from './src/rich-text-editor.component';
-export { Toolbar } from './src/toolbar.component';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { SlateLit } from './slate-lit.component';
+export { RichTextEditor } from './rich-text-editor.component';
+export { Toolbar } from './toolbar.component';
